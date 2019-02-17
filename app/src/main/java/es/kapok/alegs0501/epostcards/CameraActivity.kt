3f2899b8ef2c5b5483f3b7d913c46cb315d3444b
alegs0501 <alegs0501@gmail.com>
2019-02-17 16:41:53 -0500

    preview resizing ok

--- app/src/main/java/es/kapok/alegs0501/epostcards/CameraActivity.kt
+++ app/src/main/java/es/kapok/alegs0501/epostcards/CameraActivity.kt
@@ -2,6 +2,7 @@ package es.kapok.alegs0501.epostcards
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Point
 import android.hardware.Camera
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -10,6 +11,13 @@ import android.view.View
 import android.view.WindowManager
 import android.widget.FrameLayout
 import es.kapok.alegs0501.epostcards.models.CameraPreview
+import java.nio.file.Files.size
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import android.view.Display
+import kotlinx.android.synthetic.main.activity_camera.*
+import java.util.*
+
 
 class CameraActivity : AppCompatActivity() {
 
@@ -26,6 +34,7 @@ class CameraActivity : AppCompatActivity() {
        if (checkCameraHardware(this)){
            //create an instance of camera
            mCamera = getCameraInstance()
+           adjustPreview()
 
            mPreview = mCamera?.let {
                //Create a preview view
@@ -58,4 +67,25 @@ class CameraActivity : AppCompatActivity() {
         }
     }
 
+    /** Change width of camera preview to fit image on it*/
+    fun adjustPreview(){
+        //Get dimension of the screen
+        val display: Display = windowManager.defaultDisplay
+        val displayWidth = display.width
+        val displayHeight = display.height
+        val camWidth = mCamera!!.parameters.previewSize.width
+        val camHeight = mCamera!!.parameters.previewSize.height
+
+        //Set camera preview width by rule of 3
+        val newWidth = camWidth * displayHeight / camHeight
+
+        if (newWidth < displayWidth){
+            camera_preview.layoutParams.width = newWidth
+        }
+
+    }
+
+
+
 }
+
