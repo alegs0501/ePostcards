83cbb458f83c79cdd7aef14379251ef3d3529f45
alegs0501 <alegs0501@gmail.com>
2019-03-26 11:53:58 +0100

    animation

--- app/src/main/java/es/kapok/alegs0501/epostcards/ViewPostcardActivity.kt
+++ app/src/main/java/es/kapok/alegs0501/epostcards/ViewPostcardActivity.kt
@@ -10,8 +10,11 @@ import es.kapok.alegs0501.epostcards.models.PostcardReference
 import kotlinx.android.synthetic.main.activity_view_postcard.*
 import android.graphics.Bitmap
 import android.graphics.Matrix
+import android.opengl.Visibility
+import android.util.Log
 import android.view.MotionEvent
 import android.view.animation.*
+import kotlinx.android.synthetic.main.back_card.*
 
 
 class ViewPostcardActivity : AppCompatActivity() {
@@ -51,34 +54,102 @@ class ViewPostcardActivity : AppCompatActivity() {
 
     //Animate Postcard
     private fun animateCard(showing: Boolean){
-        val setFront = AnimationSet(true)
-        val setBack = AnimationSet(true)
-        var controllerFront = LayoutAnimationController(setFront, 0f)
-        var controllerBack = LayoutAnimationController(setBack, 0f)
 
-        var animationFront: Animation = if (!showing) {
-            ScaleAnimation(0f, 1f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        if(showing){
+            postcard_back.visibility = View.GONE
+
+            val setFront = AnimationSet(true)
+            var controllerFront = LayoutAnimationController(setFront, 0f)
+
+
+            var animationFront: Animation = if (!showing) {
+                ScaleAnimation(0f, 1f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            } else {
+                ScaleAnimation(1f, 0f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            }
+
+            animationFront.duration = 500
+            setFront.addAnimation(animationFront)
+
+
+            front_layout.layoutAnimation = controllerFront
+            front_layout.startAnimation(animationFront)
+
+            front_layout.layoutAnimation.animation.setAnimationListener(object : Animation.AnimationListener{
+                override fun onAnimationRepeat(animation: Animation?) {
+                    Log.d("TAG", "onAnimationRepeat")
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    postcard_back.visibility = View.VISIBLE
+                    val setBack = AnimationSet(true)
+                    var controllerBack = LayoutAnimationController(setBack, 500f)
+                    var animationBack: Animation = if (showing) {
+                        ScaleAnimation(0f, 1f, 1f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+                    } else {
+                        ScaleAnimation(1f, 0f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+                    }
+                    animationBack.duration = 500
+                    setBack.addAnimation(animationBack)
+                    back_layout.layoutAnimation = controllerBack
+                    back_layout.startAnimation((animationBack))
+                }
+
+                override fun onAnimationStart(animation: Animation?) {
+                    Log.d("TAG", "onAnimationStart")
+                }
+
+
+            })
         } else {
-            ScaleAnimation(1f, 0f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            postcard_front.visibility = View.GONE
+
+            val setBack = AnimationSet(true)
+            var controllerBack = LayoutAnimationController(setBack, 0f)
+
+
+            var animationBack: Animation = if (!showing) {
+                ScaleAnimation(1f, 0f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            } else {
+                ScaleAnimation(0f, 1f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+            }
+
+            animationBack.duration = 500
+            setBack.addAnimation(animationBack)
+
+
+            back_layout.layoutAnimation = controllerBack
+            back_layout.startAnimation(animationBack)
+
+            back_layout.layoutAnimation.animation.setAnimationListener(object : Animation.AnimationListener{
+                override fun onAnimationRepeat(animation: Animation?) {
+                    Log.d("TAG", "onAnimationRepeat")
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    postcard_front.visibility = View.VISIBLE
+                    val setFront = AnimationSet(true)
+                    var controllerFront = LayoutAnimationController(setFront, 500f)
+                    var animationFront: Animation = if (!showing) {
+                        ScaleAnimation(0f, 1f, 1f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+                    } else {
+                        ScaleAnimation(1f, 0f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+                    }
+                    animationFront.duration = 500
+                    setFront.addAnimation(animationFront)
+                    front_layout.layoutAnimation = controllerFront
+                    front_layout.startAnimation((animationFront))
+                }
+
+                override fun onAnimationStart(animation: Animation?) {
+                    Log.d("TAG", "onAnimationStart")
+                }
+
+
+            })
         }
 
-        var animationBack: Animation = if (showing) {
-            ScaleAnimation(0f, 1f, 1f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-        } else {
-            ScaleAnimation(1f, 0f, 1f, 1f,  Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
-        }
 
-        animationFront.duration = 500
-        animationBack.duration = 500
-
-        setFront.addAnimation(animationFront)
-        setBack.addAnimation(animationBack)
-
-        front_layout.layoutAnimation = controllerFront
-        front_layout.startAnimation(animationFront)
-
-        back_layout.layoutAnimation = controllerBack
-        back_layout.startAnimation((animationBack))
     }
 
     /**Front/Back show/hide*/
